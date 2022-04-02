c2bf06b3dc47af72a01351033b3c47329cee237d
Danny <tekhexrax@yahoo.com>
2022-04-02 12:00:55 -0400

    Updated js file working with highcharts

--- assets/script.js
+++ assets/script.js
@@ -83,6 +83,8 @@ function getWeather(city) {
       
       // For Loop to display weather info the 5 cards on the page.
       let weekForecast = []
+      let weekDailyHigh = []
+      let weekDailyLow = []
       for (i = 1; i < 6; i++) {
         
         $("#weather-card-" + i).empty();
@@ -107,7 +109,8 @@ function getWeather(city) {
         $("#weather-card-" + i).append(img, p1, p2, p3);
     
         weekForecast.push(dailyDate)
-        console.log(weekForecast)
+        weekDailyHigh.push(dailyHigh)
+        weekDailyLow.push(dailyLow)
       };
       
       Highcharts.chart('container', {
@@ -115,10 +118,10 @@ function getWeather(city) {
             type: 'spline'
         },
         title: {
-            text: 'Monthly Average Temperature'
+            text: 'Five Day Forecast'
         },
         subtitle: {
-            text: 'Source: WorldClimate.com'
+            text: 'Source: Open Weather API'
         },
         xAxis: {
             categories: [weekForecast]
@@ -147,28 +150,18 @@ function getWeather(city) {
             }
         },
         series: [{
-            name: 'Tokyo',
+            name: 'Daily High',
             marker: {
                 symbol: 'square'
             },
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
-                y: 26.5,
-                marker: {
-                    symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
-                }
-            }, 23.3, 18.3, 13.9, 9.6]
+            data: [weekDailyHigh]
     
         }, {
-            name: 'London',
+            name: 'Daily Low',
             marker: {
                 symbol: 'diamond'
             },
-            data: [{
-                y: 3.9,
-                marker: {
-                    symbol: 'url(https://www.highcharts.com/samples/graphics/snow.png)'
-                }
-            }, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            data: [weekDailyLow]
         }]
     });
 
